831e2953132dca1f242b0332898cc9b94a54f6ab
Hasan Yahya <hasna786yahya@gmail.com>
2025-01-08 19:12:38 +0500

    Refactor layout for balance display and improve text truncation for better readability

--- app/index.tsx
+++ app/index.tsx
@@ -38,17 +38,6 @@ export default function Screen() {
         <>
             <SafeAreaView className='flex-1 justify-start items-center bg-black'>
                 <ScrollView className='flex-1 w-full' contentContainerClassName='justify-start'>
-                    {/*<View className="absolute top-0 left-0 w-full h-64 bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 rounded-b-[40px] z-0"></View>
-                    <View className="absolute top-32 left-1/2 transform -translate-x-1/2 w-72 h-72 bg-indigo-300 rounded-full filter blur-3xl opacity-20 z-0"></View>*/}
-
-                    {/*<View className='mt-4 w-[86vw] relative aspect-[5/3] bg-white rounded-3xl shadow-xl items-center' //  transition-colors duration-300 ease-in-out
-                    >
-
-                        <Text className='text-center absolute top-5 left-4 text-2xl text-black font-sans'>
-                            Total Balance
-                        </Text>
-
-                    </View>*/}
 
                     <View className="p-6 flex flex-row justify-between items-start mt-3">
                         <View className="flex flex-col">
@@ -81,7 +70,7 @@ export default function Screen() {
                             <ChevronDown className='text-white mt-[0.2rem]' size={16} strokeWidth={2} />
                         </TouchableOpacity>
                         <TouchableOpacity>
-                            <Search className="text-white" size={20} strokeWidth={2} />
+                            <Search className="text-white mt-[0.2rem]" size={20} strokeWidth={2} />
                         </TouchableOpacity>
                     </View>
 
@@ -89,29 +78,29 @@ export default function Screen() {
 
                         <View className="flex gap-4 mb-4 flex-row">
                             <View className="flex-1 bg-[#FF6B6B]/30 rounded-full px-4 py-3">
-                                <View className="flex flex-row items-center gap-2">
-                                    <View className="w-12 h-12 rounded-full bg-[#FF6B6B] flex items-center justify-center">
+                                <View className="flex flex-row items-center gap-4">
+                                    <View className="w-14 h-14 rounded-full bg-[#FF6B6B] flex items-center justify-center">
                                         <ArrowDown className="w-4 h-4 text-white" />
                                     </View >
                                     <View >
-                                        <Text className="text-sm text-gray-400">Spending</Text>
-                                        <Text className="text-lg font-semibold">$1,021</Text>
+                                        <Text className="text-sm text-gray-400 truncate">Spending</Text>
+                                        <Text className="text-lg font-semibold truncate">$1,021</Text>
                                     </View >
                                 </View >
                             </View >
                             <View className="flex-1 bg-[#4CAF50]/30 rounded-full px-4 py-3">
-                                <View className="flex flex-row items-center gap-2">
-                                    <View className="w-12 h-12 rounded-full bg-[#4CAF50] flex items-center justify-center">
+                                <View className="flex flex-row items-center gap-4">
+                                    <View className="w-14 h-14 rounded-full bg-[#4CAF50] flex items-center justify-center">
                                         <ArrowUp className="w-4 h-4 text-white" />
                                     </View >
                                     <View >
-                                        <Text className="text-sm text-gray-400">Income</Text>
-                                        <Text className="text-lg font-semibold">$10,000</Text>
+                                        <Text className="text-sm text-gray-400 truncate">Income</Text>
+                                        <Text className="text-lg font-semibold truncate">$10,000</Text>
                                     </View >
                                 </View >
                             </View >
                         </View >
-                        <View className='flex px-auto items-center justify-center'>
+                        <View className='flex px-auto items-center justify-center mt-2'>
                             <View className="inline-block items-center justify-center px-4 py-2 rounded-full bg-gray-800 mx-auto">
                                 <Text className="text-sm truncate">Balance: $8,979</Text>
                             </View>
