e63571a29092b81cf4bfc020105067f6fcd36117
Hasan Yahya <hasna786yahya@gmail.com>
2025-01-08 17:41:12 +0500

    Update TouchableOpacity margin in Screen component for improved layout

--- app/index.tsx
+++ app/index.tsx
@@ -53,7 +53,7 @@ export default function Screen() {
                         </View >
                         <View className="flex items-center gap-4">
                             {/* <Search className="w-6 h-6 text-gray-400" /> */}
-                            <TouchableOpacity className="w-16 h-16 mt-0.5 rounded-full overflow-hidden">
+                            <TouchableOpacity className="w-16 h-16 mt-0 rounded-full overflow-hidden">
                                 <Image
                                     src="https://fastly.picsum.photos/id/1079/200/200.jpg?hmac=1ufYwVqTHDtGZw0aD-rsTU5gv74qWxm5-k7xQYkSeig"
                                     alt="Profile"
